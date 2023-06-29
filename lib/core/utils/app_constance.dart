f65f527de0c9e78482a3a8d4fe4a4f104e87d158
Andrew Sameh <andrewsameh22@hotmail.com>
2023-06-29 17:17:31 +0300

    first commit

---
+++ lib/core/utils/app_constance.dart
@@ -0,0 +1,4 @@
+class AppConstances {
+  static const baseurl = "https://api.themoviedb.org/3";
+  static const apiKey = "46e11de373f0f984146aef4aa74f327a";
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Movies());
+}
+
+class Movies extends StatelessWidget {
+  const Movies({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
