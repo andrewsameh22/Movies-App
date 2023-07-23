8d1a2de86a388f5d65185750585401900b66a1ba
Andrew Sameh <andrewsameh22@hotmail.com>
2023-07-23 19:10:11 +0300

    Movie Details Models

---
+++ lib/movies/data/models/movie_details_model.dart
@@ -0,0 +1,28 @@
+import 'package:movies/movies/data/models/genres_model.dart';
+import 'package:movies/movies/domain/entities/movie_details.dart';
+
+class MovieDetailsModel extends MovieDetails {
+  MovieDetailsModel({
+    required super.backdropPath,
+    required super.id,
+    required super.overview,
+    required super.release_date,
+    required super.runtime,
+    required super.title,
+    required super.voteAverage,
+    required super.genres,
+  });
+
+  factory MovieDetailsModel.fromJson(Map<String, dynamic> json) =>
+      MovieDetailsModel(
+        backdropPath: json["backdrop_path"],
+        id: json["id"],
+        overview: json["overview"],
+        release_date: json["release_date"],
+        runtime: json["runtime"],
+        title: json["title"],
+        voteAverage: json["vote_average"],
+        genres: List<GenresModel>.from(
+            json["genres"].map((x) => GenresModel.fromJson(x))),
+      );
+}
